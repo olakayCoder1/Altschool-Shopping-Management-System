d50aa18477381597df562dacc4284a524285bb30
olakayCoder1 <programmerolakay@gmail.com>
2022-09-04 23:03:19 +0100

    make decrease_item_quantity and add_item_to_items_category a class method

--- store.py
+++ store.py
@@ -1,57 +1,56 @@
 
-items_category = {
-    "men's clothing": {
-        'items': []
-    },
-
-    "women's clothing": {
-        'items': [] 
-    },
-    "electronics": {
-        'items': [] 
-    },
-}
-
-
-
-
-def add_item_to_items_category( name , price , category , id ):
-    if items_category.get(str(category)) != None :
-        category_dict = items_category.get(str(category))
-        if len(category_dict['items']) > 0 :
-            for item_object in category_dict['items'] :
-                if item_object['name'] == name :
-                    item_object['quantity'] += 1 
-                    item_object['price'] = price 
-                    return
-            new_item = dict()
-            new_item['name'] = name
-            new_item['price'] = price
-            new_item['quantity'] = 10
-            new_item['id'] = id
-            items_category.get(str(category))['items'].append(new_item)
-        else:
-            new_item = dict()
-            new_item['name'] = name
-            new_item['price'] = price
-            new_item['id'] = id 
-            new_item['quantity'] = 10
-            items_category.get(str(category))['items'].append(new_item) 
-
-
-
-def decrease_item_quantity(category,id):
-    for item in items_category[category]['items'] :
-        if item['id'] == int(id) :
-            item['quantity'] -= 1
-
 class Item:
+
+
+    items_category = {
+            "men's clothing": {
+                'items': []
+            },
+
+            "women's clothing": {
+                'items': [] 
+            },
+            "electronics": {
+                'items': [] 
+            },
+        }
+        
     def __init__(self , category , name , price = 10.00 ):
         self.name = name 
         self.price = price
         self.id = 0
         self.category = category
         self.next_item = None
+
+    @classmethod
+    def decrease_item_quantity(cls , category , id):
+        for item in cls.items_category[category]['items'] :
+            if item['id'] == int(id) :
+                item['quantity'] -= 1
+
+    @classmethod
+    def add_item_to_items_category( cls , name , price , category , id ):
+        if cls.items_category.get(str(category)) != None :
+            category_dict = cls.items_category.get(str(category))
+            if len(category_dict['items']) > 0 :
+                for item_object in category_dict['items'] :
+                    if item_object['name'] == name :
+                        item_object['quantity'] += 1 
+                        item_object['price'] = price 
+                        return
+                new_item = dict()
+                new_item['name'] = name
+                new_item['price'] = price
+                new_item['quantity'] = 10
+                new_item['id'] = id
+                cls.items_category.get(str(category))['items'].append(new_item)
+            else:
+                new_item = dict()
+                new_item['name'] = name
+                new_item['price'] = price
+                new_item['id'] = id 
+                new_item['quantity'] = 10
+                cls.items_category.get(str(category))['items'].append(new_item) 
 
 
 
@@ -76,16 +75,18 @@ class Store:
         if self.head == None:
             new_item.id = self.items_length() + 1
             self.head = new_item
-            add_item_to_items_category(name, price , category , id=new_item.id)
-            items_category[category] 
+            new_item.add_item_to_items_category(name, price , category , id=new_item.id)
+            new_item.items_category[category] 
             return
         current_node = self.head
         while current_node.next_item != None :
             current_node = current_node.next_item
         current_node.next_item = new_item
         new_item.id = self.items_length() 
-        add_item_to_items_category(name, price , category, id=new_item.id)
+        new_item.add_item_to_items_category(name, price , category, id=new_item.id)
   
+
+
     def print_items(self):
         if self.head == None :
             return None
@@ -96,26 +97,17 @@ class Store:
         print(f'{current_node.id} | {current_node.name} | {current_node.price}$ ' )
         return
 
+
     def display_search(self, result : list ):
         for items in result:
             print(f"{ items['id'] } | { items['name'] } | { items['price']}")
 
 
     def search_by_category(self,query):
-        # search_result = [] 
-        # for category in items_category.keys():
-        #     if category.find(query) != -1 :
-        #         for item in items_category[category]['items']:
-        #             search_result.append(item)
-        # if len(search_result) <= 0 :
-        #     print('We do not have item in your search category\nThese are the categories we have: ')
-        #     return
-        # else:
-        #     self.display_search(search_result)
-        
 
-        if items_category.get(str(query)) != None :
-            self.display_search(items_category[query]['items'])
+        custom_instance = Item( name='' , price=0 , category='' )
+        if custom_instance.items_category.get(str(query)) != None :
+            self.display_search(custom_instance.items_category[query]['items'])
             return
         else:
             print('We do not have item in your search category\nThese are the categories we have: ')
@@ -127,11 +119,11 @@ class Store:
         current_node = self.head
         while current_node.next_item != None :
             if current_node.id == id :
-                decrease_item_quantity(current_node.category , current_node.id )
+                current_node.decrease_item_quantity(current_node.category , current_node.id )
                 return {'id': current_node.id  , 'name' : current_node.name , 'price' : current_node.price }
             current_node = current_node.next_item
         if current_node.id == id :
-            decrease_item_quantity(current_node.category , current_node.id )
+            current_node.decrease_item_quantity(current_node.category , current_node.id )
             return {'id': current_node.id  , 'name' : current_node.name , 'price' : current_node.price }
         return
 
